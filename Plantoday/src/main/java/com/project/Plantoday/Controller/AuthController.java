8e6433eb27511b336d960d2e04594d6eefb2df32
qkrdudwls <129957194+qkrdudwls@users.noreply.github.com>
2024-06-06 17:31:35 +0900

    Update AuthController 회원가입 실패 시 다시 회원가입 페이지로, 로그인 실패 시 다시 로그인 페이지로.

--- Plantoday/src/main/java/com/project/Plantoday/Controller/AuthController.java
+++ Plantoday/src/main/java/com/project/Plantoday/Controller/AuthController.java
@@ -6,6 +6,7 @@ import com.project.Plantoday.exception.InvalidCredentialsException;
 import com.project.Plantoday.exception.UserAlreadyExistsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,7 +14,41 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    @PostMapping("/register")
+    public String register(@ModelAttribute UserDTO userDTO, Model model) {
+        try {
+            authService.register(userDTO);
+            model.addAttribute("successMessage", "User registered successfully");
+            return "login";
+        } catch (UserAlreadyExistsException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "register";
+        }
+    }
 
+    @GetMapping("/register")
+    public String registerPage(Model model) {
+        model.addAttribute("userDTO", new UserDTO());
+        return "register";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute UserDTO userDTO, Model model) {
+        try {
+            authService.login(userDTO.getUsername(), userDTO.getPassword());
+            return "redirect:/home";
+        } catch (InvalidCredentialsException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "login";
+        }
+    }
+
+    @GetMapping("/login")
+    public String loginPage(Model model) {
+        model.addAttribute("userDTO", new UserDTO());
+        return "login";
+    }
+/*
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody UserDTO userDTO) {
         try {
@@ -43,5 +78,5 @@ public class AuthController {
     public String loginPage(){
         return "login";
     }
-
+*/
 }
