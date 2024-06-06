825d83e7c34d3846483e6c72bce676899328852c
qkrdudwls <129957194+qkrdudwls@users.noreply.github.com>
2024-06-06 19:05:07 +0900

    Change AuthController

--- Plantoday/src/main/java/com/project/Plantoday/Controller/AuthController.java
+++ Plantoday/src/main/java/com/project/Plantoday/Controller/AuthController.java
@@ -6,6 +6,10 @@ import com.project.Plantoday.exception.InvalidCredentialsException;
 import com.project.Plantoday.exception.UserAlreadyExistsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody UserDTO userDTO) {
@@ -34,6 +39,10 @@ public class AuthController {
     public ResponseEntity<?> login(@RequestBody UserDTO userDTO) {
         try {
             authService.login(userDTO.getUsername(), userDTO.getPassword());
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(userDTO.getUsername(), userDTO.getPassword())
+            );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
             return ResponseEntity.ok("User logged in successfully");
         } catch (InvalidCredentialsException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
